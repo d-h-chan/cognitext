dacb96e6868bb3213afd3304abf3e7b8bc2c5163
Daniel Chan <dchan@Daniels-Air.hsd1.ca.comcast.net>
2024-03-19 17:08:32 -0700

    added prisma generate postinstall script

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
     config.resolve.alias.canvas = false
     config.resolve.alias.encoding = false
     return config
-  }
+  },
 };
 
 export default nextConfig;
--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,46 +1,44 @@
-"use client";
+"use client"
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { trpc } from "../_trpc/client";
-import { Loader2 } from "lucide-react";
-import React from "react";
+import { Suspense, useEffect } from 'react'; // Import useEffect
+import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
 
 // 'auth-callback' route, shows loading spinner and redirects based on status
 const Page = () => {
-  const router = useRouter();
-
-  const searchParams = useSearchParams();
-
-  // get the origin for redirection as a param from the url, e.g. "/auth-callback?origin=dashboard"
-  const origin = searchParams.get("origin");
-
-  const { isPending, status, error } = trpc.authCallback.useQuery(undefined, {
+  const router = useRouter()
+  // Store the query result in a variable
+  const { data, error, isSuccess, isError } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
     retryDelay: 500,
   });
 
-  if (isPending) {
-    return (
-      <div className="w-full mt-24 flex justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-          <h3 className="font-semibold text-xl">Setting up your account...</h3>
-          <p>You will be redirected automatically.</p>
-        </div>
+  // Handle success in useEffect
+  useEffect(() => {
+    if (isSuccess && data?.success) {
+      router.push('/dashboard');
+    }
+  }, [isSuccess, data, router]);
+
+  // Handle error in useEffect
+  useEffect(() => {
+    if (isError && error.data?.code === 'UNAUTHORIZED') {
+      router.push('/sign-in');
+    }
+  }, [isError, error, router]);
+
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>
+          Setting up your account...
+        </h3>
+        <p>You will be redirected automatically.</p>
       </div>
-    );
-  }
+    </div>
+  )
+}
 
-  if (error?.data?.code === "UNAUTHORIZED") {
-    //this doesnt go anywhere yet
-    router.push("/sign-in");
-  }
-
-  if (status === "success") {
-    router.push(origin ? `/${origin}` : "/dashboard");
-  }
-
-  // shouldn't reach here
-  return <></>;
-};
-
-export default Page;
+export default Page
